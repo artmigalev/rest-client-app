e6da9aadd66e573e22bdfedeb34e8836809e6d54
artmigalev <art_migalev@yahoo.com>
2025-09-04 19:15:50 +0300

    fix: deploy folder

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,16 +1,7 @@
-import Header from '~/layout/header';
-import Navigation from '~/navigation/Navigation';
-
 export function meta() {
   return [{ title: 'Postman Clone' }];
 }
 
 export default function Home() {
-  return (
-    <>
-      <Header>
-        <Navigation />
-      </Header>
-    </>
-  );
+  return <></>;
 }
