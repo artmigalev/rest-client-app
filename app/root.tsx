81311d439508c7d23a2d91adec333c4fc0514ce0
artmigalev <art_migalev@yahoo.com>
2025-09-13 13:20:04 +0300

    feat: add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -14,10 +14,8 @@ import { Provider } from 'react-redux';
 import { store } from './state-management/store';
 import { Suspense } from 'react';
 import Loader from './components/Loader';
-
-export const links: LinksFunction = () => [
-  { rel: 'icon', href: '/postman_128px_6C5DF7.svg', type: 'image/svg+xml' },
-];
+// eslint-disable-next-line react-refresh/only-export-components
+export { meta, links } from './root.meta';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -25,7 +23,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        
+
         <Meta />
         <Links />
       </head>
--- react-router.config.ts
+++ react-router.config.ts
@@ -1,25 +1,7 @@
 import type { Config } from '@react-router/dev/config';
-import type { Route } from './.react-router/types/app/+types/root';
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   // basename:'/postman-clone',
   ssr: false,
 } satisfies Config;
-export function meta() {
-  return [{ title: 'Postman Clone' }];
-}
-export const links: Route.LinksFunction = () => [
-  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-  {
-    rel: 'preconnect',
-    href: 'https://fonts.gstatic.com',
-    crossOrigin: 'anonymous',
-  },
-  {
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
-  },
-
-
-];
