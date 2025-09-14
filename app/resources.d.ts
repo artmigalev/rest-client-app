f133a75019eb63acbed4872e75dca5bfeeb4c7e0
artmigalev <art_migalev@yahoo.com>
2025-09-14 10:54:10 +0300

    feat: declare register

--- app/resources.d.ts
+++ app/resources.d.ts
@@ -1,33 +1,33 @@
 import 'i18next';
 
-interface Resources {
-  header: {
-    login: 'Войти';
-    register: 'Зарегистрироваться';
-  };
-  login: {
-    button: {
-      label: 'Войти';
-    };
-    password: {
-      label: 'Пароль';
-      maxLength: 'Имя пользователя должно быть не более 20 букв';
-      minLength: 'Имя пользователя должно быть не менее 8 букв';
-      placeholder: 'Введите пароль';
-      required: 'Пароль обязателен';
-    };
-    username: {
-      label: 'Имя пользователя';
-      maxLength: 'Имя пользователя должно быть не более 20 символов';
-      minLength: 'Имя пользователя должно быть не менее 3 символов';
-      placeholder: 'Введите имя пользователя';
-      required: 'Имя пользователя обязательно';
-    };
-  };
-  welcome: {
-    greeting: 'Welcome!';
-  };
-}
+// interface Resources {
+//   header: {
+//     login: 'Войти';
+//     register: 'Зарегистрироваться';
+//   };
+//   login: {
+//     button: {
+//       label: 'Войти';
+//     };
+//     password: {
+//       label: 'Пароль';
+//       maxLength: 'Имя пользователя должно быть не более 20 букв';
+//       minLength: 'Имя пользователя должно быть не менее 8 букв';
+//       placeholder: 'Введите пароль';
+//       required: 'Пароль обязателен';
+//     };
+//     username: {
+//       label: 'Имя пользователя';
+//       maxLength: 'Имя пользователя должно быть не более 20 символов';
+//       minLength: 'Имя пользователя должно быть не менее 3 символов';
+//       placeholder: 'Введите имя пользователя';
+//       required: 'Имя пользователя обязательно';
+//     };
+//   };
+//   welcome: {
+//     greeting: 'Welcome!';
+//   };
+// }
 
 declare module 'i18next' {
   interface CustomTypeOptions {
@@ -53,6 +53,56 @@ declare module 'i18next' {
           minLength: string;
           placeholder: string;
           required: string;
+        };
+      };
+      firstName: {
+        label: string;
+        placeholder: string;
+      };
+      lastName: {
+        label: string;
+        placeholder: string;
+      };
+      username: {
+        label: string;
+        placeholder: string;
+      };
+      password: {
+        label: string;
+
+        placeholder: string;
+      };
+      button: {
+        label: string;
+      };
+      error: {
+        firstName: {
+          message: {
+            required: string;
+            minLength: string;
+            maxLength: string;
+          };
+        };
+        lastName: {
+          message: {
+            required: string;
+            minLength: string;
+            maxLength: string;
+          };
+        };
+        username: {
+          message: {
+            required: string;
+            minLength: string;
+            maxLength: string;
+          };
+        };
+        password: {
+          message: {
+            required: string;
+            minLength: string;
+            maxLength: string;
+          };
         };
       };
       welcome: {
