9ad3bfb56792fe31f22f59ac9990506f4ba65403
artmigalev <art_migalev@yahoo.com>
2025-10-20 20:44:40 +0300

    feat: 404

---
+++ app/routes/NotFound.tsx
@@ -0,0 +1,9 @@
+function NotFound() {
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <img className="w-full h-full " src="404.jpg" alt="not-found" />
+    </div>
+  );
+}
+
+export default NotFound;
