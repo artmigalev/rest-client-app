def1123c5e9cea61cea945960d290f460f5276c3
artmigalev <art_migalev@yahoo.com>
2025-10-24 18:12:55 +0300

    feat:langSlice

---
+++ app/reducers/langSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import type { RootState } from '~/store';
+
+type lang = string;
+
+const initialState = {
+  val: 'en' as lang
+}
+
+const langSlice = createSlice({
+  name: 'language',
+  initialState,
+  reducers: {
+    setByLang: (state, action: PayloadAction<string>) => {
+      state.val = action.payload;
+    },
+  },
+});
+
+export const { setByLang } = langSlice.actions;
+
+export const selectLang = (state: RootState) => state.lang.val;
+
+export default langSlice.reducer;
