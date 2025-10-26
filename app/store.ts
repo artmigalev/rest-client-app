423660a092573a16526de90f488a2c5ea1cedcd1
artmigalev <art_migalev@yahoo.com>
2025-10-26 11:53:44 +0300

    toolkit

--- app/store.ts
+++ app/store.ts
@@ -1,19 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import userReducer from './reducers/userSlice'
+import userReducer from './reducers/userSlice';
 
-import langReducer from './reducers/langSlice'
+import langReducer from './reducers/langSlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    lang:langReducer
+    lang: langReducer,
   },
 });
 
+export type AppStore = typeof store;
 
-export type AppStore = typeof store
+export type RootState = ReturnType<AppStore['getState']>;
 
-export type RootState = ReturnType<AppStore['getState']>
-
-export type AppDispatch = AppStore['dispatch']
+export type AppDispatch = AppStore['dispatch'];
