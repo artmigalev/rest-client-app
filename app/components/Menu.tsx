faddd0ea6dece44be8acef1c583beb44d111ea76
artmigalev <art_migalev@yahoo.com>
2025-10-30 23:03:18 +0300

    feat: menu

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,17 +1,27 @@
-import React from "react";
-import type { lang } from "~/reducers/langSlice";
+import React, { useEffect } from 'react';
+import type { lang } from '~/reducers/langSlice';
+import List from './List';
+import { useTranslation } from 'react-i18next';
 
-interface IMenu{
+interface IMenu {
   props: {
-    lang: lang,
-    menuItems: 
-  }
+    lang: lang;
+  };
 }
 
+function Menu({ lang }: IMenu['props']) {
+  const { t, i18n } = useTranslation('menu');
+  const menuItems = Object.entries(t('menu', { returnObjects: true }));
 
-function Menu({lang, menuItems}) {
+  useEffect(() => {
+    i18n.changeLanguage(lang);
+  }, [lang]);
 
-  return <div>Menu</div>;
+  return (
+    <>
+      <List items={menuItems} styles='justify-around p-3' isLink={true} />
+    </>
+  );
 }
 
 export default Menu;
