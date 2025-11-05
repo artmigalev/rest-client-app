694f8809010876c492d1ae09a4a75a2e925ef168
artmigalev <art_migalev@yahoo.com>
2025-11-05 19:19:45 +0300

    add: custom errors

---
+++ app/components/errors/erros-auth.ts
@@ -0,0 +1,8 @@
+import { FirebaseError } from "firebase/app";
+
+export class AuthUserError extends FirebaseError{
+  constructor(code:string, message:string) {
+    super(code, message)
+    this.message =message
+  }
+}
