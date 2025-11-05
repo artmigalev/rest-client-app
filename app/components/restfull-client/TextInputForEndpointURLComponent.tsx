08fef578a0e53b4cf832d74f5e99f7cfcf20e50d
artmigalev <art_migalev@yahoo.com>
2025-11-05 23:03:15 +0300

    feat: text Input and MethodSelector

--- app/components/restfull-client/TextInputForEndpointURLComponent.tsx
+++ app/components/restfull-client/TextInputForEndpointURLComponent.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { type ChangeEvent, type Dispatch } from 'react';
+import type { IRestFullClient } from '~/routes/RestFullClient';
 
-function TextInputForEndpointURLComponent() {
+interface ITextInputForEndpointURLComponent {
+  defaultValue: string;
+  dispatcher: Dispatch<React.SetStateAction<IRestFullClient['options']>>;
+}
+
+function TextInputForEndpointURLComponent({ defaultValue, dispatcher }: ITextInputForEndpointURLComponent) {
+  const prefix = 'http://';
+
+  const onHandleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    let inputValue = event.target.value;
+
+    dispatcher((state) => ({ ...state, textInput: inputValue }));
+  };
+
   return (
-    <div className='field flex flex-row items-center gap-1'>
-      <label htmlFor=''>Endpoint</label>
-      <div className='border w-max p-1 rounded-[8px]'>
-        <input className='border bg-gray-500' />
-        <button type='submit' className='p-1'>
-          Go
-        </button>
-      </div>
+    <div className='flex justify-between items-center w-[270px] h-12 rounded-xl relative border-gray-400 border-2'>
+      <span className='flex items-center justify-center text-input-prefix text-lg h-full w-[70px] font-semibold p-2.5 bg-background rounded-bl-xl rounded-tl-xl'>
+        {prefix}
+      </span>
+      <input
+        defaultValue={defaultValue}
+        onChange={onHandleChange}
+        className='flex items-center justify-center outline-0 font-medium border-0  bg-input-bg py-2.5 w-40 text-sm'
+        placeholder='Endpoint'
+      />
+      <span className='text-4xl bg-input-bg h-full w-10 flex items-center justify-center pr-2.5 rounded-t-xl rounded-b-xl cursor-pointer relative pb-3'>
+        📨
+      </span>
     </div>
   );
 }
