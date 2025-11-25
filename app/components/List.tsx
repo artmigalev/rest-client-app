2e19d4c71bcd582077542c21ad02640a7209f822
artmigalev <art_migalev@yahoo.com>
2025-11-25 21:55:21 +0300

    add: fixed components

--- app/components/List.tsx
+++ app/components/List.tsx
@@ -1,31 +1,56 @@
 import { Link, type Path } from 'react-router';
 
-interface IList<T> {
-  items: [key: T, string][];
-  isLink: boolean;
-  styles?: string;
-  callbackFn?: () => void | undefined;
+
+
+type stylesList = {
+
+  classItems?: string
+  classList: string
+  stylesForLinks?:string
+  stylesForBtn?:string
+
+
 }
 
-function List<T>({ items, isLink, styles, callbackFn }: IList<T>) {
-  const children = items.map(([key, val]) => (
-    <li key={key as string}>
-      {isLink ? (
-        <Link
-          className='capitalize w-max p-1'
-          to={key === 'logout' ? '/' : (key as Partial<Path>)}
-          onClick={key === 'logout' ? callbackFn : undefined}
-          viewTransition
-        >
-          {val}
-        </Link>
-      ) : (
-        <span>{val as string}</span>
-      )}
-    </li>
-  ));
+interface IList<T> {
+  items: [key: T, string][] | string[];
+  styles?: stylesList;
+  callbackFn?: () => void | undefined;
+  returnedTypeItems: 'links' | 'btn' | '';
+}
 
-  return <ul className={`w-full flex flex-row items-center ${styles}`}>{children}</ul>;
+function List<T>({ items, styles, callbackFn, returnedTypeItems }: IList<T>) {
+
+
+  let children;
+  switch (returnedTypeItems as IList<T>['returnedTypeItems']) {
+    case 'btn':
+      children = items.map((item) => (
+        <li  className={styles?.classItems} key={Date.now()}>
+          <button className={`${styles?.stylesForBtn}  cursor-pointer`} type='button'>
+            {item as string}
+          </button>
+        </li>
+      ));
+      break;
+    case 'links':
+      children = items.map(([key, val]) => (
+        <li  className={styles?.classItems} key={key as string}>
+          <Link id='link'
+            className={`${styles?.stylesForLinks} capitalize w-max p-1`}
+            to={key === 'logout' ? '/' : (key as Partial<Path>)}
+            onClick={key === 'logout' ? callbackFn : undefined}
+            viewTransition
+          >
+            {val as string}
+          </Link>
+        </li>
+      ));
+    default:
+      break;
+  }
+
+  return <ul className={`w-full flex flex-row items-center ${styles?.classList}`}>{children}</ul>;
 }
 
 export default List;
