19c7b30af83ba1dd84934d50492e964df4710f61
artmigalev <art_migalev@yahoo.com>
2025-11-27 07:49:54 +0300

    init: history component

--- app/routes/History.tsx
+++ app/routes/History.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { returnedFirestore } from '~/firebase/apicalls';
 
 function History() {
-  return <div className='absolute top-2/4'>History</div>;
+  let history;
+
+
+  const getHistory = async () => {
+    history = await returnedFirestore()
+    console.log('history returned');
+
+
+  }
+
+
+
+  return <section className='p-4 relative w-full h-full'>
+    <div>
+      <button onClick={getHistory} className='p-2.5 border bg-amber-300'>get history</button>
+    </div>
+    <div className='h-[100px] border '>
+      <h2>history</h2>
+      <pre>
+        { history}
+      </pre>
+    </div>
+  </section>;
 }
 
 export default History;
