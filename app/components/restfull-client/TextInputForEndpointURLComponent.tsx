d9987c304c70ee0f6c603987ca9841f2e3960930
artmigalev <art_migalev@yahoo.com>
2025-12-07 21:08:52 +0300

    add: value url

--- app/components/restfull-client/TextInputForEndpointURLComponent.tsx
+++ app/components/restfull-client/TextInputForEndpointURLComponent.tsx
@@ -1,12 +1,12 @@
 import React, { type ChangeEvent, type Dispatch } from 'react';
 import { useFetcher } from 'react-router';
 
-function TextInputForEndpointURLComponent() {
+function TextInputForEndpointURLComponent({ value }:{value:string}) {
   return (
     <div id='container'>
       <label htmlFor='url-endpoint'>URL</label>
       <div className='flex flex-row w-full'>
-        <input className='input-url' type='text' name='url-endpoint' id='' />
+        <input defaultValue={value} className='input-url' type='text' name='url-endpoint' id='' />
         <button type='submit' className='btn-endpoint'>Search</button>
       </div>
     </div>
